ea878dbfa5b660faaafc9089524645f95e3696dc
Max <max.alt@gmx.de>
2022-12-19 16:27:10 +0100

    new structure implementation

---
+++ ringbuffer.py
@@ -0,0 +1,43 @@
+class MyCircularQueue():
+
+    def __init__(self, k):
+        self.k = k
+        self.queue = [1] * k
+        self.head = self.tail = -1
+
+    # Insert an element into the circular queue
+    def enqueue(self, data):
+
+        if ((self.tail + 1) % self.k == self.head):
+            for i in range(self.k -1):
+                self.queue[i] = self.queue[i+1]
+            self.queue[self.k -1] = data    
+            #print("The circular queue is full\n")
+
+        elif (self.head == -1):
+            self.head = 0
+            self.tail = 0
+            self.queue[self.tail] = data
+        else:
+            self.tail = (self.tail + 1) % self.k
+            self.queue[self.tail] = data
+    
+    def get(self):
+        return self.queue[:]
+    def len(self):
+        return len(self.queue)    
+    
+    def printCQueue(self):
+        if(self.head == -1):
+            print("No element in the circular queue")
+
+        elif (self.tail >= self.head):
+            for i in range(self.head, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
+        else:
+            for i in range(self.head, self.k):
+                print(self.queue[i], end=" ")
+            for i in range(0, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
--- visu_analog.py
+++ visu_analog.py
@@ -12,24 +12,29 @@ from matplotlib.figure import Figure
 import tkinter as tk
 from tkinter import ttk
 
+from functools import partial
+from ringbuffer import MyCircularQueue 
 LARGE_FONT=("Verdana",12)
 style.use("ggplot")
 
-fig = Figure(figsize=(5,5),dpi =100)
-ax1 = fig.add_subplot(1,1,1)
-print("setup fig")
 
-def animate(i):
+def animate(frame, application, page):
+    
     x_ = []
-    for x in range(queue.len()):
+    for x in range(application.getqueue(page).len()):
         x_.append(x)
     
-    if ser.inWaiting() > 0:
-            data = ser.read(5)
-           # print("recieved: "+ str(data)[2:7])
-            queue.enqueue(float(str(data)[2:7]))
+    try: 
+        if application.getisConnected(page):
+            if application.getserialCommunication(page).inWaiting() > 0:
+                    data = application.getserialCommunication(page).read(5)
+                    application.enqueue(page,float(str(data)[2:7]))
+    except:
+        pass
+    
+    ax1 = application.getplot(page)
     ax1.clear()  
-    ax1.plot(x_,queue.get())          
+    ax1.plot(x_,application.getqueuevalue(page))          
             
 class visu(tk.Tk):
     
@@ -45,91 +50,96 @@ class visu(tk.Tk):
         container.grid_columnconfigure(0, weight =1)
         
         self.frames = {}
-        for F in(StartPage,PageOne):
-            frame = F(container, self)
-            self.frames[F]= frame
-            frame.grid(row=0, column =0, sticky = "nsew")
-        self.show_frame(StartPage)
-    
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+        frame = StartPage(container, self)
+        self.frames[StartPage]= frame
+        frame.grid(row=0, column =0, sticky = "nsew")
         frame.tkraise()
-
+        
+    def getserialCommunication(self,con) :
+        return self.frames[con].getserialCommunication()
+    
+    def getisConnected(self,con):
+        return self.frames[con].getisConnected()
+    
+    def getfig(self,con):
+        return self.frames[con].getfig()
+    
+    def getplot(self,con):
+        return self.frames[con].getplot()
+    
+    def getqueue(self,con):
+        return self.frames[con].getqueue()
+    
+    def enqueue(self,con,value):
+        self.frames[con].enqueue(value)
+    
+    def getqueuevalue(self,con):
+        return self.frames[con].getqueuevalue()
+        
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
+        self.serialCommunication = None
+        self.isConnected = False
+        self.fig = Figure(figsize=(5,5),dpi =100)
+        self.ax1 = self.fig.add_subplot(1,1,1)
+        self.queue = MyCircularQueue(200)
+        
         tk.Frame.__init__(self,parent)
         label = tk.Label(self,text="StartPage", font = LARGE_FONT)
         label.pack(pady=10,padx=10)
         
-        button1 =ttk.Button(self, text="visualization",
-                           command = lambda: controller.show_frame(PageOne))
-        button1.pack()
-        
-class PageOne(tk.Frame):
-    def __init__(self,parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self,text="Visualization: Analog Signals", font = LARGE_FONT)
-        label.pack(pady=10,padx=10)
-        button1 =ttk.Button(self, text="backtohome",
-                           command = lambda: controller.show_frame(StartPage))
-        button1.pack()
+        buttonconnect =ttk.Button(self, text="connect",
+                         command = self.connect)
+        buttonconnect.pack()
 
-        canvas = FigureCanvasTkAgg(fig,self)
+        button_disconnect = button1 =ttk.Button(self, text="disconnect",
+                         command = self.disconnect)
+        button_disconnect.pack()
+        
+        canvas = FigureCanvasTkAgg(self.fig,self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP,fill=tk.BOTH, expand=True)
-            
-            
-class MyCircularQueue():
-
-    def __init__(self, k):
-        self.k = k
-        self.queue = [1] * k
-        self.head = self.tail = -1
-
-    # Insert an element into the circular queue
-    def enqueue(self, data):
-
-        if ((self.tail + 1) % self.k == self.head):
-            for i in range(self.k -1):
-                self.queue[i] = self.queue[i+1]
-            self.queue[self.k -1] = data    
-            #print("The circular queue is full\n")
-
-        elif (self.head == -1):
-            self.head = 0
-            self.tail = 0
-            self.queue[self.tail] = data
-        else:
-            self.tail = (self.tail + 1) % self.k
-            self.queue[self.tail] = data
     
-    def get(self):
-        return self.queue[:]
-    def len(self):
-        return len(self.queue)    
+    def connect(self):
+        try:
+            self.serialCommunication = serial.Serial('COM5',56000)
+            print("serial com established")
+            print(self.serialCommunication)
+            self.isConnected = True
+        except:
+            print("Connection not possible !!!")
     
-    def printCQueue(self):
-        if(self.head == -1):
-            print("No element in the circular queue")
-
-        elif (self.tail >= self.head):
-            for i in range(self.head, self.tail + 1):
-                print(self.queue[i], end=" ")
-            print()
-        else:
-            for i in range(self.head, self.k):
-                print(self.queue[i], end=" ")
-            for i in range(0, self.tail + 1):
-                print(self.queue[i], end=" ")
-            print()
+    def disconnect(self):
+        try:
+            self.serialCommunication.close()
+            print("disconnected!!")
+            self.isConnected = True
+        except:
+            print("Cant disconnect")    
+    def getisConnected(self):
+        return self.isConnected
             
-global queue
+    def getserialCommunication(self):
+        return self.serialCommunication  
+    
+    def getfig(self):
+        return self.fig  
+    
+    def getplot(self):
+        return self.ax1
+                     
+    def getqueue(self):
+        return self.queue
+    
+    def enqueue(self,con):
+        self.queue.enqueue(con)
+
+    def getqueuevalue(self):
+        return self.queue.get()
+                
 
 if __name__ == "__main__":
-    #run()
-    queue = MyCircularQueue(250)
-    ser = serial.Serial('COM5',56000)
-    print("serial com established")
+
     app = visu()
-    ani = animation.FuncAnimation(fig, animate, interval = 10)
+    ani = animation.FuncAnimation(app.getfig(StartPage), partial(animate, application = app, page = StartPage), interval = 10)
     app.mainloop()
